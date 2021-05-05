4acab4c8ff6cbdf83fbf922ab0ce460df995a05f
Anureet Kaur <anureetkaur8286@gmail.com>
2021-05-05 22:43:07 +0530

    updation

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\testController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +19,5 @@ Route::get('/', function () {
 });
 
 Route::get('queries',[UserController::class,'index']);
+Route::get('demo',[testController::class,'index']);
+Route::view('test','test');
